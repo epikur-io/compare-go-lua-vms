3eacd365698d09b4558dffb7473c4eb37d07208d
Epikur <3p1kur@protonmail.ch>
2025-03-03 09:38:38 +0100

    added script to install dependenciews / go tools

---
+++ deps.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+GO_BIN_PATH="$(go env GOPATH)/bin"
+if [[ ":$PATH:" == *":$GO_BIN_PATH:"* ]]; then
+  echo "Your path is correctly set"
+else
+  echo "Your path is missing ~/bin, you might want to add it."
+fi
+
+go install github.com/go-task/task/v3@v3.41.0
